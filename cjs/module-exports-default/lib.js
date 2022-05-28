b7a4796e665ef62b605ec5476a50a5e41408e89e
Roman Rodin <romaro83@gmail.com>
2022-05-29 00:37:12 +0300

    add cjs export default

---
+++ cjs/module-exports-default/lib.js
@@ -0,0 +1,7 @@
+const c = 'c';
+const d = {
+    name: 'Ivan'
+};
+
+module.exports.default = c;
+module.exports.blabla = d;
--- esm/import-from-ejs/app.js
+++ esm/import-from-ejs/app.js
@@ -1,3 +1,12 @@
 import { a } from '../../cjs/module-exports/lib.js';
+import c from '../../cjs/module-exports-default/lib.js';
+import d from '../../cjs/module-exports-default/lib.js';
 
 console.log(a);
+
+/**
+ * Все cjs экспорты вида module.exports.some приходят в esm упакованными в объект со свойством some
+ * Если экспортов несколько, они сольются в один объект
+ */
+console.log(c);
+console.log(d);
